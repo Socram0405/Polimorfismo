5c6cae02e005792ab3a98048452bf40f08f0a3cc
Socram0405 <marcoscaatillo400@gmail.com>
2025-06-04 22:22:57 -0400

    Implementacion del header rectangulo

--- rectangulo.h
+++ rectangulo.h
@@ -1,10 +1,26 @@
 #ifndef RECTANGULO_H
 #define RECTANGULO_H
 
-class rectangulo
-{
+#include "Forma.h"
+
+class rectangulo : public Forma {
+private:
+    int base, altura;
+
 public:
     rectangulo();
+    rectangulo(int X, int Y, int _base, int _altura);
+
+    void setRectangulo(int _base, int _altura);
+    
+    int getBase();
+    int getAltura();
+
+    int perimetro() override;
+    int area() override;
 };
+
+void CaseRectanguloArea(rectangulo r);
+void CaseRectanguloPerim(rectangulo r);
 
 #endif // RECTANGULO_H
